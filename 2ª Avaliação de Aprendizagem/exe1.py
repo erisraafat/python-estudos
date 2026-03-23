160aedbd39c5ab2dd6e66cfd365b49906d4815b3
𓋹 Éris Raafat 𓋹 <164649119+erisraafat@users.noreply.github.com>
2026-03-23 16:17:12 -0300

    Add files via upload

---
+++ 2ª Avaliação de Aprendizagem/exe1.py
@@ -0,0 +1,11 @@
+valor1 = int(input("Valor1:"))
+valor2 = int(input("Valor2:"))
+valor3 = int(input("Valor3:"))
+
+if valor1 > valor2 and valor3:
+    print(valor1,"é o maior")
+elif valor3 > valor2 and valor1:
+    print(valor3,"é o maior")
+elif valor2 > valor1 and valor3:
+    print(valor2,"é o maior")
+    
